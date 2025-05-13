e2072904ef4bb924ebc99a36e61f6497ca216775
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-05-13 10:22:04 +0000

    temporizador con botones

---
+++ src/js/components/Box.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Box = ({number}) => {
+
+    return (
+        <>
+        <div className="number"><span>{number}</span></div>
+        </>
+    )
+
+}
+
+export default Box;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,51 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Box from "./Box.jsx";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+ const [timer, setTimer] = useState(0);
+ const [active, setActive] = useState(false);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	useEffect(() => {
+		if(active){
+			const interval = setInterval(() => {
+			setTimer(num => num + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+		}
+		
+		
+	},[timer,active])
+	
+	const resetTimer = () =>{
+		setTimer(value=> value = 0);
+	}
+
+	return (
+		<>
+		<div className="text-center">
+			<div className="counter">
+				<Box number="⏱️"/>
+			<Box number={Math.floor(timer/100000)%10}/>
+			<Box number={Math.floor(timer/10000)%10}/>
+			<Box number={Math.floor(timer/1000)%10}/>
+			<Box number={Math.floor(timer/100)%10}/>
+			<Box number={Math.floor(timer/10)%10}/>
+			<Box number={Math.floor(timer%10)}/>
+			</div>
+            
+			<div className="buttons-container">
+				<button onClick={() => setActive(true)} className="mx-2 btn btn-success">Empezar</button>
+				<button onClick={() => setActive(false)} className="mx-2 btn btn-danger">Parar</button>
+				<button onClick={resetTimer} className="mx-2 btn btn-dark">Reset</button>
+			</div>
+
 		</div>
+
+		</>
 	);
 };
 
